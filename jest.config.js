bac7fbcaf99227839a4799aa814cfd98cf094285
tianbo <tianboqiu@gmail.com>
2020-11-29 20:08:47 -0800

    init monorepos and projects

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/app',
+    '<rootDir>/apps/app-2',
+    '<rootDir>/apps/mobile',
+    '<rootDir>/apps/web',
+    '<rootDir>/apps/server',
+  ],
+};
